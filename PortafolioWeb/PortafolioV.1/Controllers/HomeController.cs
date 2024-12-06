104cec4c0719e5449bb442dde49610be11b77740
Matias <matiascalvoecheverria@gmail.com>
2024-12-06 15:20:05 -0600

    PortafolioWeb

---
+++ PortafolioWeb/PortafolioV.1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PortafolioV._1.Models;
+
+namespace PortafolioV._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult IndexEN()
+        {
+            return View("IndexEN");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PortafolioWeb/PortafolioV.1/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var check = document.querySelector(".check");
+check.addEventListener('click', idioma);
+
+function idioma() {
+	let id = check.checked;
+	if (id == true) {
+		location.href = "/Home/IndexEN";
+	} else {
+		location.href = "/Home/Index";
+	}
+}
+
+type = "text/javascript" >
+	window.addEventListener("scroll", function () {
+		var header = document.querySelector("header");
+		header.classList.toggle("abajo", window.scrollY > 0);
+	})
+
+typeidioma = "text/javascript" >
+	window.addEventListener("scroll", function () {
+		var idiomas = document.querySelector(".idiomas");
+		idiomas.classList.toggle("abajo", window.scrollY > 0);
+	})
